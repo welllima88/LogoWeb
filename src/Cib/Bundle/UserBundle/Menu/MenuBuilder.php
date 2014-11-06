f785baae957b7690207a2b7148057f582f1a6d47
cedricCIB <c.demir@cib-ingenierie.fr>
2014-11-06 09:58:27 +0100

    fis user admin and menu builder

--- src/Cib/Bundle/UserBundle/Menu/MenuBuilder.php
+++ src/Cib/Bundle/UserBundle/Menu/MenuBuilder.php
@@ -17,9 +17,26 @@ class MenuBuilder
 
     private $factory;
 
-    public function __construct(FactoryInterface $factory)
+    private $isAdmin = false;
+
+    private $user;
+
+
+    public function __construct(FactoryInterface $factory, SecurityContext $securityContext)
     {
         $this->factory = $factory;
+        $this->user = $securityContext->getToken()->getUser();
+        if($this->user != "anon.")
+        {
+            $roles = $this->user->getRoles();
+            foreach($roles as $role){
+                if($role == 'ROLE_ADMIN')
+                {
+                    $this->isAdmin = true;
+                    break;
+                }
+            }
+        }
     }
 
     public function createLoginMenu(Request $request)
@@ -34,13 +51,16 @@ class MenuBuilder
 
     public function createLoggedInMenu(Request $request, SecurityContext $securityContext)
     {
+//        var_dump($securityContext->getToken()->getUser());die;
         $menu = $this->factory->createItem('loggedin');
         $menu->setChildrenAttribute('class', 'nav navbar-nav navbar-right');
         $menu->addChild('Deconnexion',array('route' => 'fos_user_security_logout'));
-        $menu->addChild($securityContext->getToken()->getUser(),array('route' => 'fos_user_profile_show'));
-        $menu->addChild('S\'enregistrer',array('route' => 'fos_user_registration_register'));
-        $menu->addChild('Utilisateurs',array('route' => 'displayUsers'));
-
+        $menu->addChild($this->user,array('route' => 'fos_user_profile_show'));
+        if($this->isAdmin == true)
+        {
+            $menu->addChild('S\'enregistrer',array('route' => 'fos_user_registration_register'));
+            $menu->addChild('Utilisateurs',array('route' => 'displayUsers'));
+        }
         return $menu;
     }
 } 
